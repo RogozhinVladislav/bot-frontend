c47e3848389cd6feedf49e33ddce10dffc73841c
RogozhinVladislav <rogojinvladislav@gmail.com>
2019-11-30 15:30:02 +0300

    Реализация загрузки изображнеия для команд

--- src/api/commands.js
+++ src/api/commands.js
@@ -1,6 +1,10 @@
 import http from '@/utils/http'
 
 export const fetchList = () => http.get('/commands')
-export const create = command => http.post('/commands', command)
+export const create = command => http.post('/commands', command, {
+  headers: {
+    'Content-Type': 'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW',
+  },
+})
 export const update = ({ commandId, command }) => http.put(`/commands/${commandId}`, command)
 export const remove = id => http.delete(`/commands/${id}`)
